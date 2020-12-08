52edce291ce1749eb7ea65f5ae4ef517aa7a8c5b
Mike Shertz <shertzm2@gmail.com>
2020-12-08 11:17:18 -0500

    utilizing material-ui to create a tabs component for showing information about the house

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,37 @@
-import logo from '../logo.svg';
+import React from 'react';
+// import logo from '../logo.svg';
 import pic1 from '../pic1.JPEG';
 import '../App.css';
+import SimpleTabs from './Tabs.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={pic1} className="App-logo" alt="logo" />
         <h1>
           Welcome to the Turtle Bay House!
         </h1>
+        <img src={pic1} className="App-logo" alt="logo" />
         <h4>
           Your serene paradise in Sea Isle City
         </h4>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+
+
+        <SimpleTabs></SimpleTabs>
+          {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+        <p>
+          this is the body
+        </p>
+      </body>
     </div>
   );
 }
