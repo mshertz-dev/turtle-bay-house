db88e595ae616e9b46d0bae803150bad128e1af7
Mike Shertz <shertzm2@gmail.com>
2021-01-05 09:49:11 -0500

    added booking links to booking module

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -17,11 +17,14 @@ function Booking() {
 
   return (
     <div className={classes.root}>
-      <h3 id="about-details">Our booking module is in the works and will be here soon, but in the meantime, you have a couple of options for booking your stay at the Turtle Bay House in Sea Isle.</h3>
-      <br></br>
-      <h4 id="about-details">Option 1: AirBnB & VRBO</h4>
-      <br></br>
-      <h4 id="about-details">AirBnB: </h4>
+      <h3 id="about-details">Our booking module is in the works and will be here soon, but in the meantime, you have a couple of options for booking your stay at the Turtle Bay House in Sea Isle.  Links to our AirBNB and VRBO listings are below.  Feel free to also contact us directly at chrissyandmichaelshertz@gmail.com or to inquire about booking or simply fill out the form below and we'll respond shortly!</h3>
+      <div id="booking-option-1">
+        <div id="booking-option-header">Option 1: airbnb & VRBO</div>
+        <br></br>
+        <div id="booking-option-airbnb"><a href="https://www.airbnb.com/rooms/29828310?s=67&unique_share_id=df4c94b0-f5ea-458c-b09c-4b0f143af170">airbnb Listing</a></div>
+        <br></br>
+        <div id="booking-option-vrbo"><a href="https://www.vrbo.com/1875369?adultsCount=1&unitId=2437880">VRBO Listing</a></div>
+      </div>
     </div>
   );
 }
