cd7cbcee8cdea844c4675c1e58dea75ab1bb3c6b
Mike Shertz <shertzm2@gmail.com>
2021-01-05 22:05:09 -0500

    added endpoints to serve build

--- server/server.js
+++ server/server.js
@@ -7,11 +7,20 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static((__dirname + '/../public')));
+app.use(express.static((__dirname + '/../build')));
 
 //test api
 app.get('/api/hi', (req, res) => {
   res.send('hiyee');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 module.exports = app;
